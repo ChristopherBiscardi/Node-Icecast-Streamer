97336f2d3371a3d5e0ac489c7a7cffb297b7e322
ChristopherBiscardi <biscarch@sketcht.com>
2011-05-25 01:38:49 -0400

    Stations and playlists work... Streaming does not

--- www/public/script.js
+++ www/public/script.js
@@ -5,11 +5,27 @@ alert('clicked');
     $.ajax({
      type: "POST",
      url: "/station/myStation",
-     data: "name=myStation&f=create&song=01.mp3",
+     data: "name=myStation&f=create&files=~/Users/Chris/Gits/Node-Icecast-Streamer/www/01.mp3",
      success: function(msg){
        alert( "Data Saved: " + msg );
      }
    });
    //$.post("station/myStation", { name: "John", time: "2pm" } );
   })
+  
+  
+  var audio = new Audio();
+        if (audio.canPlayType('application/ogg; codecs="vorbis"') == "probably"
+            && (/Chrome/i.test(navigator.userAgent) || !/Safari/i.test(navigator.userAgent))) {
+          encoding = 'ogg';
+        } else {
+          encoding = 'mp3';
+        }
+        audio.src='/station/myStation.' + encoding;
+        audio.setAttribute('controls', true);
+        audio.setAttribute('autobuffer', false);
+      $('body').append(audio);
+        audio.load();
+        audio.play();
+      alert(encoding);
 });
